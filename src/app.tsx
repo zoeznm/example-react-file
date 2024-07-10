204f657b03afa0c4155ef7b5759d42d2c4c79817
zoeznm <bomikim046@gmail.com>
2024-07-10 14:24:18 +0900

    문제 해결 완료

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,60 @@
 import React from "react";
 
-const App :React.FC = () => {
+const App: React.FC = () => {
+  // 스타일 객체를 변수로 분리하여 재사용 가능하게 정의
+  const containerStyles: React.CSSProperties = {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const loginStyles: React.CSSProperties = {
+    width: "30vw",
+    height: "50vh",
+    backgroundColor: "#333",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
+  const inputStyles: React.CSSProperties = {
+    width: "80%",
+    height: "24px",
+    backgroundColor: "#D9D9D9",
+    margin: "10px 0",
+    textAlign: "center",
+  };
+
+  const submitStyles: React.CSSProperties = {
+    width: "80%",
+    height: "24px",
+    backgroundColor: "#9AFF92",
+    margin: "10px 0",
+    textAlign: "center",
+    cursor: "pointer",
+  };
+
+  // 클릭 이벤트 핸들러를 함수로 따로 분리하여 관리
+  const loginSubmit = () => {
+    alert("Hello");
+  };
+
   return (
-    <h1>hello</h1>
+    <div style={containerStyles}>
+      <div id="login" style={loginStyles}>
+        <div id="login-input" style={inputStyles}>
+          Input
+        </div>
+        <div id="login-submit" style={submitStyles} onClick={loginSubmit}>
+          Submit
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-let name = 'app';
+let name = "app";
 
 module.exports = {
-  entry: `./src/${name}.tsx`,
+  entry: `./src/index.tsx`,
 
   output: {
     filename: `${name}.bundle.js`,
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/,
-        use: ["style-loader","css-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js|jsx|ts|tsx$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
-      }
+      },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx', 'css'],
+    extensions: [".ts", ".js", ".tsx", ".jsx", ".css"], // 수정된 부분
   },
 };
